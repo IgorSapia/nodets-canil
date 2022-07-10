9703320b306b48bd2fe4e7f2d42c6c8405d02373
IgorSapia <igor.sapia@gmail.com>
2022-07-09 22:06:54 -0300

    Projeto Finalizado

--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,13 @@ const server = express();
 
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
-server.engine('mustahce', mustache());
+server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
 
-// Routes
 server.use(mainRoutes);
 server.use((req, res)=>{
-    res.send('pagina nâo encontrada');
+    res.render('pages/404');
 });
 
 server.listen(process.env.PORT)
